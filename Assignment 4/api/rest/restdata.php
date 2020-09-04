350e334fd3be42cce832fb69a4e975562bb5a35d
vspecky <thelonenerd9913@gmail.com>
2020-09-04 16:56:37 +0530

    Changed tabs to spaces in Assignment 4 php api

--- Assignment 4/api/rest/restdata.php
+++ Assignment 4/api/rest/restdata.php
@@ -1,38 +1,38 @@
 <?php
 class RestaurantData {
-	private $menu;
+    private $menu;
 
-	function __construct($data) {
-		$this->menu = $data;
-	}
+    function __construct($data) {
+        $this->menu = $data;
+    }
 
-	public function getItemByID($id) {
-		$res = 'Not found';
-		foreach($this->menu as $item) {
-			if ($item['id'] == $id) {
-				$res = $item;
-				break;
-			}
-		}
+    public function getItemByID($id) {
+        $res = 'Not found';
+        foreach($this->menu as $item) {
+            if ($item['id'] == $id) {
+                $res = $item;
+                break;
+            }
+        }
 
-		return json_encode($res);
-	}
+        return json_encode($res);
+    }
 
-	public function getItemsInPriceRange(float $low, float $high) {
-		$items = [];
+    public function getItemsInPriceRange(float $low, float $high) {
+        $items = [];
 
-		foreach($this->menu as $item) {
-			$price = floatval($item['price_large']);
-			if ($price >= $low && $price <= $high) {
-				array_push($items, $item);
-			}
-		}
+        foreach($this->menu as $item) {
+            $price = floatval($item['price_large']);
+            if ($price >= $low && $price <= $high) {
+                array_push($items, $item);
+            }
+        }
 
-		if (sizeof($items) === 0) {
-			$items = 'Not found';
-		}
+        if (sizeof($items) === 0) {
+            $items = 'Not found';
+        }
 
-		return json_encode($items);
-	}
+        return json_encode($items);
+    }
 }
 ?>
